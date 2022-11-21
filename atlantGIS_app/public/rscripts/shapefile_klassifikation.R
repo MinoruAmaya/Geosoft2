dd036816728ff7a268061a9e3d73eb2da06960e0
maschroder <mais1998@web.de>
2022-11-21 15:52:15 +0100

    R Dateien

---
+++ atlantGIS_app/public/rscripts/shapefile_klassifikation.R
@@ -0,0 +1,67 @@
+rm(list=ls())
+library(mapview)
+library(terra)
+library(caret)
+library('sf')
+library('sp')
+library('raster')
+
+setwd("Passendes WD finden")
+
+# Einladen des Vektordatensatzes
+Referenzdaten <- st_read("Classification.shp")
+
+###############################################################################
+
+# Working Directory ändern
+setwd("Passendes WD finden")
+
+# Kiel einladen
+sentinel <- rast("Kiel.tif")
+
+# Trainingsdaten auf die Kiel Projektion umändern
+Referenzdaten <- st_transform(Referenzdaten, crs(sentinel))
+
+Referenzdaten$Area <- "Kiel"
+
+extr <- extract(sentinel, Referenzdaten)
+head(extr)
+
+Referenzdaten$PolyID <- 1:nrow(Referenzdaten)
+
+extr <- merge(extr,Referenzdaten,by.x="ID",by.y="PolyID")
+head(extr)
+
+saveRDS(extr,file="trainData4.RDS")
+
+predictors <- c("B02","B03","B04","B08","B05","B06","B07","B11","B12","B8A","NDVI","NDVI_3x3_sd","NDVI_5x5_sd")
+
+TrainIDs <- createDataPartition(extr$ID,p=0.3,list=FALSE)
+TrainDat <- extr[TrainIDs,]
+
+TrainDat <- TrainDat[complete.cases(TrainDat[,predictors]),]
+model <- train(TrainDat[,predictors],
+               TrainDat$Label,
+               method="rf",
+               importance=TRUE,
+               ntree=500) # 50 is quite small (default=500). But it runs faster.
+
+saveRDS(model,file="RFModel2.RDS")
+
+lvls <- list(levels(model$trainingData$.outcome))
+
+prediction <- predict(as(sentinel,"Raster"),model)
+prediction_terra <- as(prediction,"SpatRaster")
+
+cols <- c( "lightgreen","blue", "green","darkred","forestgreen",
+           "darkgreen","beige","darkblue"," firebrick1","red","yellow")
+#spplot(deratify(prediction),col.regions=cols)
+plot(prediction_terra,col=cols)
+
+writeRaster(prediction_terra,"prediction2.grd",overwrite=TRUE)
+
+pdf("LULC_Kiel")
+#spplot(deratify(prediction),maxpixels=ncell(prediction)*0.4,
+#       col.regions=cols)
+plot(prediction_terra,col=cols,maxcell=ncell(prediction_terra)*0.4)
+dev.off()
