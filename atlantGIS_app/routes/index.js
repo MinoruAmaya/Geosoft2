a1cc50c4b02d044a2c4b01bd612b1ce6318f6c94
luening <he-luening@t-online.de>
2022-11-23 15:54:01 +0100

    uninstalled body-parser, jimp installed multer for file upload added multer functions changed input accept and enctype

--- atlantGIS_app/routes/index.js
+++ atlantGIS_app/routes/index.js
@@ -4,6 +4,62 @@ var router = express.Router();
 const fs = require("fs");
 const R = require('r-integration');
 
+const multer = require('multer')
+
+
+//multer
+var satelliteImageStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+
+    // Uploads is the Upload_folder_name
+    cb(null, "./public/data/satelliteimage")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + ".tif")
+  }
+})
+
+var trainingdataStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+
+    // Uploads is the Upload_folder_name
+    cb(null, "./public/data/trainingdata")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + ".tif")
+  }
+})
+
+
+
+var uploadSatelliteImage = multer({
+  storage: satelliteImageStorage,
+  limits: { fileSize: 1000000000 },
+  fileFilter: function (req, file, cb) {
+
+    console.log("test");
+
+    // Set the filetypes, it is optional
+    var filetypes = /tif/;
+    var mimetype = filetypes.test(file.mimetype);
+
+    var extname = filetypes.test(path.extname(
+      file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    cb("Error: File upload only supports the "
+      + "following filetypes - " + filetypes);
+  }
+
+  // mypic is the name of file attribute
+}).single("req.body.satellitenbild");
+
+
+
+
 //GET home page
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Startseite" });
@@ -11,11 +67,23 @@ router.get("/", function (req, res, next) {
 
 
 router.post("/upload_satellitenbild", function (req, res, next) {
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  uploadSatelliteImage(req, res, function (err) {
 
+    if (err) {
 
-  
+      // ERROR occurred (here it can be occurred due
+      // to uploading image of size greater than
+      // 1MB or uploading different file type)
+      res.send(err)
+    }
+    else {
 
-  res.render("workflow_train", { title: "Satelitenbild hinzugefügt", data: JSON.stringify(test) });
+      // SUCCESS, image successfully uploaded
+      res.render("workflow_sat", { title: "Satelitenbild hinzugefügt"});
+    }
+  })
 });
 
 
