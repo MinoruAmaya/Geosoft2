f46bcaf8064fd0bf0b5c91063f0d007ee7c42304
Maximilian Elfers <maximilian-elfers@gmx.de>
2022-11-23 14:02:44 +0100

    impressum seite hinzugefügt

--- atlantGIS_app/routes/impressum.js
+++ atlantGIS_app/routes/impressum.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('impressum', { title: 'Hier wird die analyse Seite entstehen wo die Ergebnisse visualisiert werden und die Daten gedownlaoded werden können' });
+  res.render('impressum', { title: 'Hier wird die impressum Seite entstehen' });
 });
 
 module.exports = router;
