94568d547e8b80ce22f50b57460f17bead10c20a
maschroder <mais1998@web.de>
2022-12-05 16:00:10 +0100

    geopackage to geojson

---
+++ atlantGIS_app/public/rscripts/geopackageToGeojson.R
@@ -0,0 +1,18 @@
+# R Scripts, um Daten im Geopackage-Format in GeoJson umzuwandeln
+
+convertGeoPackageToGeoJson <- function(filename, filepath) {
+    # setwd(filepath)
+
+    library(sf)
+    library(geojson)
+    library(geojsonsf)
+
+
+    trainData_sf <- read_sf(paste(filepath, paste(filename, ".gpkg", sep=""), sep=""))
+    trainData_sf_4326 <- st_transform(trainData_sf, crs = st_crs("EPSG:4326"))
+    trainData_geojson <- sf_geojson(trainData_sf_4326)
+
+    geo_write(trainData_geojson, paste(filepath, paste(filename, ".geojson", sep=""), sep=""))
+
+    return("Successfully converted the training data from GeoPackage to GeoJSON")
+}
