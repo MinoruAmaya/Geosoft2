ae716e1b30c19521461eb635e8722606c75e6adc
Maximilian Elfers <maximilian-elfers@gmx.de>
2022-12-21 14:23:59 +0100

    file changes

--- atlantGIS_app/Backend/public/rscripts/trainModell.R
+++ atlantGIS_app/Backend/public/rscripts/trainModell.R
@@ -1,15 +1,13 @@
 library(terra)
 library(raster)
 library(sf)
-library(lattice)
-library(ggplot2)
 library(caret)
-train_data <- read_sf("./public/data/demo/Muenster_Demo_Training.gpkg")
-predictors <- c("B02", "B03", "B04", "B08", "B05", "B06", "B07", "B11",
-            "B12", "B8A")
-model <- train(train_data[, predictors],
-           train_data$Label,
-           method = "rf",
-           importance = TRUE,
-           ntree = 500)
+train_data <- read_sf("./public/data/demo/Muenster_Demo_Training.gpkg");
+predictors <- c("B02","B03","B04","B08","B05","B06","B07","B11",
+            "B12","B8A")
+model <- train(train_data[,predictors],
+        train_data$Label,
+        method = "rf",
+        importance = TRUE,
+        ntree = 500)
 return(model)
