44ac303ada65a8718b94c547484e7f815f798e5d
Maximilian Elfers <maximilian-elfers@gmx.de>
2023-01-11 12:13:46 +0100

    angepasst and script aufbau

--- atlantGIS_app/Backend/database/rscripts/geopackageToGeojson.R
+++ atlantGIS_app/Backend/database/rscripts/geopackageToGeojson.R
@@ -1,10 +1,14 @@
 # R Scripts, um Daten im Geopackage-Format in GeoJson umzuwandeln
-library(sf)
-library(geojson)
-library(geojsonsf)
-train_data_sf <- read_sf(paste(filepath,
-    paste(filename, ".gpkg", sep = ""), sep = ""))
-train_data_sf_4326 <- st_transform(trainData_sf, crs = st_crs("EPSG:4326"))
-train_data_geojson <- sf_geojson(trainData_sf_4326)
-geo_write(train_data_geojson, paste(filepath,
-    paste(filename, ".geojson", sep = ""), sep = ""))
+geopackage_to_geojson <- function() {
+
+    library(sf)
+    library(geojson)
+    library(geojsonsf)
+
+    train_data_sf <- read_sf(paste(filepath,
+        paste(filename, ".gpkg", sep = ""), sep = ""))
+    train_data_sf_4326 <- st_transform(trainData_sf, crs = st_crs("EPSG:4326"))
+    train_data_geojson <- sf_geojson(trainData_sf_4326)
+    geo_write(train_data_geojson, paste(filepath,
+        paste(filename, ".geojson", sep = ""), sep = ""))
+}
