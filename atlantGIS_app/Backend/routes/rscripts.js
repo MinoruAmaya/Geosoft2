f11220cc806697eaf65d812eb563464a45930587
Maximilian Elfers <maximilian-elfers@gmx.de>
2023-01-15 16:55:36 +0100

    correct method

--- atlantGIS_app/Backend/routes/rscripts.js
+++ atlantGIS_app/Backend/routes/rscripts.js
@@ -3,7 +3,7 @@ const R = require('r-integration');
 var router = express.Router();
 
 router.get('/trainModell', function(req, res, next) {
-    R.callMethod("./database/rscripts/trainModell.R", "trainModell").then((result) => {
+    R.callMethod("./database/rscripts/trainModell.R", "trainModell", {area: "Enschede", algorithm: "rf"}).then((result) => {
       console.log(result);
       res.render('index', { title: resut });
     }).catch((error) => {
