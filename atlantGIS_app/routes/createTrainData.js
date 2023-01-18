c6b483b44c6904a74d3c91979d1777a0db4630b6
luening <he-luening@t-online.de>
2023-01-18 12:31:21 +0100

    trainingdata upload maybe fixed

--- atlantGIS_app/routes/createTrainData.js
+++ atlantGIS_app/routes/createTrainData.js
@@ -2,16 +2,21 @@ var express = require('express');
 const multer = require('multer');
 var router = express.Router();
 
+// global attributes
+let fileURL = "database/trainingdata/"
+let fileName;
+let fileType
+
 
 // multer storage -------------------------------------------------------------------------------------------------------
 // trainingDataStorage
-let fileURL = "database/trainingdata/"
 var trainingDataStorage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, fileURL);
   },
   filename: function (req, file, callback) {
-    fileName = file.originalname;
+    fileType = file.originalname.toString().split(".")[1];
+    fileName = "trainingdata" + fileType;
     callback(null, fileName);
   }
 });
@@ -31,9 +36,7 @@ router.get('/', function (req, res, next) {
 // route to trainModel
 // fetch not testet so not working ---------
 router.post("/uploadTrainingData", uploadTrainingData.single("training"), function (req, res, next) {
-  file = fileURL + fileName
-  let extension = file.split('.').pop();
-  if (extension.toLowerCase() == 'gpkg') {
+  if (fileType.toLowerCase() == 'gpkg') {
     fetch("http://backend:4000/geopackageToGeoJSON");
   }
   res.render('createTrainData');
