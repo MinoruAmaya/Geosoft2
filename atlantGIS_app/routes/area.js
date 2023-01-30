95299ccf15327df76d76a39bf046c9356f7fa33d
Maximilian Elfers <maximilian-elfers@gmx.de>
2023-01-30 13:15:41 +0100

    Workflow angepasst und Satellitenbild in Map eingefügt

--- atlantGIS_app/routes/area.js
+++ atlantGIS_app/routes/area.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 //routes ---------------------------------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('area');
+  res.render('area', {help: [1]});
 });
 
 // route to satelliteimage if area is saved in @areaJS.js
 router.post("/addData", function (req, res, next) {
-  res.render('addTrainData');
+  res.render('addTrainData', {help: [1]});
 })
 
 module.exports = router;
